1d662793069ce81550633f37fd55653769120f7e
Rudra Pratap <rudrapratap@edu.sait.ca>
2025-04-17 22:19:33 -0600

    working on errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@
 
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const inter = Inter({ subsets: ['latin'] });
 
